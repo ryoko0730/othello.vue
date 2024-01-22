a3e181aa60f9a61442473acbfcff427651cd8b2c
ryoko0730 <yoshihara2021@patholab-meiji.jp>
2024-01-22 18:46:08 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/othello.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+align-content: center;
+}
+</style>
---
+++ src/components/othello.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="othelloBoard">
+    <div class="container">
+      <div class="board">
+        <tr v-for="cell in cells" :key="cell">
+          <div ></div>
+        </tr>
+      </div>
+    </div>
+  </div>
+  <div><button @click="restart()"></button></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      cells: [
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,1,-1,0,0,0],
+        [0,0,0,-1,1,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0]
+      ]
+    }
+  },
+  methods: {
+    restart() {
+
+}
+}
+}
+</script>
+
+<style>
+.othelloBoard {
+  align-content: center;
+  display: inline-block;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  width: 430px;
+  height: 470px;
+  background: #333;
+}
+
+.board {
+  display: flex;
+  align-items: center;
+  border: #333;
+  border-collapse:collapse;
+  width: 408px;
+  height: 408px;
+  background: darkgreen;
+}
+
+.cell {
+  table-layout: fixed;
+  width: 50px;
+  height: 48px;
+}
+
+.disc {
+  width: 85%;
+  height: 85%;
+  border-radius: 50%;
+}
+
+.white {
+  background: #eee;
+}
+
+.black {
+  background: #333;
+}
+</style>
